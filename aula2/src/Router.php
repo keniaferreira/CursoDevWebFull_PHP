3e484ab4ba4150abacc636384ef8390fd82d817b
Kênia Ferreira <keniaferreira@live.com>
2021-02-06 15:28:27 -0300

    Aula 2 - Sistema de Rotas com MVC

---
+++ aula2/src/Router.php
@@ -0,0 +1,53 @@
+<?php
+ 
+namespace Src;
+ 
+use Src\Request;
+use Src\Dispatcher;
+use Src\RouteCollection;
+ 
+class Router 
+{
+     
+    protected $route_collection;
+ 
+    public function __construct()
+    {
+ 
+        $this->route_collection = new RouteCollection;
+        $this->dispacher = new Dispatcher;
+    }
+ 
+    public function get($pattern, $callback)
+    {
+         
+        $this->route_collection->add('get', $pattern, $callback);
+        return $this;
+    }
+ 
+    public function post($pattern, $callback)
+    {
+         
+        $this->route_collection->add('post', $pattern, $callback);
+        return $this;   
+    }
+ 
+    public function put($pattern, $callback)
+    {
+         
+        $this->route_collection->add('put', $pattern, $callback);
+        return $this;   
+    }
+ 
+    public function delete($pattern, $callback)
+    {
+         
+        $this->route_collection->add('delete', $pattern, $callback);
+        return $this;   
+    }
+ 
+    public function find($request_type, $pattern)
+    {
+        return $this->route_collection->where($request_type, $pattern);
+    }
+}
